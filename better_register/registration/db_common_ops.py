bb44e99f34a5b3062f400ce75f050b114c98c87d
Tanner Baldus <t.baldus285@gmail.com>
2015-02-03 14:45:47 -0800

    made update_m2m a pure function

--- better_register/registration/db_common_ops.py
+++ better_register/registration/db_common_ops.py
@@ -1,14 +1,13 @@
 __author__ = 'tanner'
 
 
-
-def update_nonkey_fields(model_instance, field_names, **kwargs):
+def update_simple_fields(model_instance, field_names, **kwargs):
     count = 0
     for attr_name in field_names:
-        attribute = getattr(model_instance,attr_name)
+        attribute = getattr(model_instance, attr_name)
         new_attr = kwargs[attr_name]
         if new_attr != attribute:
-            setattr(model_instance,attr_name, new_attr)
+            setattr(model_instance, attr_name, new_attr)
             count += 1
     return count
 
@@ -25,26 +24,18 @@ def update_m2m(m2m_field, model_class, pk_field, model_kwargs_list):
     :param model_kwargs_list: a list of dicts mapping model_class fieldnames to values
     :return: the number of relationships changed
     """
-    old_models = {getattr(i, pk_field):i for i in m2m_field.all() if i.pk_field not in model_kwargs_list}
+    old_models = {getattr(i, pk_field): i for i in m2m_field.all() if i.pk_field not in model_kwargs_list}
 
-    if not model_kwargs_list and m2m_field.all():
-        m2m_field.clear()
-        return 1
+    new_relations = 0
+    updated_models = []
 
-    else:
-        new_relations = 0
-        updated_models = []
+    for model_dict in model_kwargs_list:
 
-        for model_dict in model_kwargs_list:
+        if model_dict.get(pk_field) not in old_models:
+            new_model = model_class.objects.get_or_create(**model_dict)
+            updated_models.append(new_model)
+            new_relations += 1
 
-            if model_dict.get(pk_field) not in old_models:
-                new_model = model_class.objects.get_or_create(**model_dict)
-                updated_models.append(new_model)
-                new_relations += 1
+    return new_relations,updated_models,old_models
 
-        if new_relations > 0:
-            m2m_field.remove(*old_models.values())
-            m2m_field.add(*updated_models)
-
-        return new_relations
 
