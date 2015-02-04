6dfd5cfb7ff59576f557477e3befe49fa86e8379
Tanner Baldus <t.baldus285@gmail.com>
2015-02-03 16:16:59 -0800

    changed update_m2m to actually update the m2m relation using _get_new_old function

--- better_register/registration/db_common_ops.py
+++ better_register/registration/db_common_ops.py
@@ -12,10 +12,25 @@ def update_simple_fields(model_instance, field_names, **kwargs):
     return count
 
 
-def update_m2m(m2m_field, model_class, pk_field, model_kwargs_list):
+def _filter_model_pk_mapping(models, pk_field, pk_value_lst):
     """
-      Takes a list of kwargs defining the value of fields and updates the, m2m _field to have relationships to models
-      of type model_class with the fields.
+
+    :param models:
+    :param pk_field:
+    :param pk_value_lst:
+    :param lst_good:
+    :return:
+    """
+    pair_pk_model = lambda model, pk_field: (getattr(model ,pk_field), model)
+    check_pk_field = lambda model, pk_field, lst: getattr(model,pk_field) not in lst
+
+    return dict(pair_pk_model(m, pk_field) for m in models if check_pk_field(m, pk_field, pk_value_lst))
+
+
+def _get_new_old(m2m_field, model_class, pk_field, model_kwargs_list):
+    """
+      Takes a list of kwargs defining the value of fields of the new models to add to relation.
+      Goes through the m2m field and finds the old models that needs to be removed.
 
 
     :param m2m_field:a many_to_many manager
@@ -24,18 +39,41 @@ def update_m2m(m2m_field, model_class, pk_field, model_kwargs_list):
     :param model_kwargs_list: a list of dicts mapping model_class fieldnames to values
     :return: the number of relationships changed
     """
-    old_models = {getattr(i, pk_field): i for i in m2m_field.all() if i.pk_field not in model_kwargs_list}
 
-    new_relations = 0
+    new_pk_values = [i.get(pk_field) for i in model_kwargs_list]
+    all_models = m2m_field.all()
+    to_be_removed = _filter_model_pk_mapping(all_models, pk_field, new_pk_values)
+
+    # keep the good models already in the relation
+    current_good_pks = [getattr(i, pk_field) for i in all_models if i not in to_be_removed.values()]
     updated_models = []
 
     for model_dict in model_kwargs_list:
-
-        if model_dict.get(pk_field) not in old_models:
+        if model_dict.get(pk_field) not in current_good_pks:
             new_model = model_class.objects.get_or_create(**model_dict)
             updated_models.append(new_model)
-            new_relations += 1
 
-    return new_relations,updated_models,old_models
+    return updated_models, to_be_removed.values()
+
+
+def update_m2m(m2m_field, model_class, pk_field, model_kwargs_list):
+    """
+
+    :param m2m_field:
+    :param model_class:
+    :param pk_field:
+    :param model_kwargs_list:
+    :return: number of relationships changed
+    """
+
+    new_models, old_models = _get_new_old(m2m_field, model_class,pk_field, model_kwargs_list)
+
+    if not new_models and not old_models:
+        return 0
+
+    m2m_field.remove(*old_models)
+    m2m_field.add(*new_models)
+
+    return len(new_models)+len(old_models)
 
 
