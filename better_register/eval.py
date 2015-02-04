ad174c02435a8becdb197de741287c51563969ab
Tanner Baldus <t.baldus285@gmail.com>
2015-02-04 01:03:03 -0800

    removed old site

--- better_register/eval.py
+++
@@ -1,9 +0,0 @@
-# coding: utf-8
-import registration.models as m
-import registration.data_entry as de
-from data import eval
-y = [i.__unicode__() for i in m.Course.objects.all()]
-for i in eval:
-    c,n,ins,r =i
-    de.create_eval(ins,c,n,r)
-    
--- better_register/registration/parsers/uoregon/subject_codes.py
+++
@@ -1,8 +0,0 @@
-__author__ = 'tanner'
-from ..common_ops import get_labeled_rows
-
-def parse_subject_table(soup):
-    table = soup.find(id="DataTables_Table_0")
-    labels = ['code', 'subject']
-    return get_labeled_rows(labels, table)
-
