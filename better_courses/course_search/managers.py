9fd3bb1528f12caee05f99f897d0332be09d65a9
Tanner Baldus <t.baldus285@gmail.com>
2015-02-18 01:16:15 -0800

    added meetings.get_or_create, finished up course.get_or_create  and made both of them return tuples to stay in line with django get_or_create api

--- better_courses/course_search/managers.py
+++ better_courses/course_search/managers.py
@@ -3,6 +3,7 @@ from django.db import models
 from django.db import connection
 from django.db.models import Avg
 from django.db.models import Q
+import  db_common_ops
 import operator
 
 class OfferingManager(models.Manager):
@@ -12,6 +13,8 @@ class OfferingManager(models.Manager):
 
     def search_by_instructor(self, fname, lname):
         return self.filter(instructor__fname=fname, instructor__lname=lname)
+
+
 
     def search_by_evaluation(self, rating, eval_rating_fn):
         """
@@ -26,8 +29,7 @@ class OfferingManager(models.Manager):
         query = reduce(operator.or_, queries)
         return self.filter(query)
 
-
-    def create_offering(self, crn, course, meetings, instructor, term, **kwargs):
+    def create_offering(self, crn, course, meetings, instructors, term, **kwargs):
         """
 
         :param crn:
@@ -35,15 +37,16 @@ class OfferingManager(models.Manager):
         :param meetings:
         :return:
         """
-        from models import Meeting, Course, Instructor, Offering, Term
-        course = Course.objects.get_or_create(**course)
+        from models import Meeting, Course, Instructor, Term
         term = Term.objects.get_or_create(**term)
-        instructor = Instructor.get_or_create(**instructor)
-        offering = Offering(crn=crn, **kwargs)
+        course = Course.objects.get_or_create(**course)
+        offering = self.create(crn=crn,term=term,course=course,**kwargs)
+        added_m2m_fields = 0
+        added_m2m_fields+= db_common_ops.add_kwargs_to_m2m(offering.instructors, Instructor.objects, instructors)
+        added_m2m_fields+= db_common_ops.add_kwargs_to_m2m(offering.meetings,  Meeting.objects, meetings)
+        if added_m2m_fields:
+            offering.save()
 
-        for m in meetings:
-            meeting = Meeting.objects.get_or_create(**m)
-            offering.meetings.add(meeting)
 
 
 class EvaluationManager(models.Manager):
@@ -75,20 +78,35 @@ class InstructorManager(models.Manager):
         return self.filter(id__in=instructors)
 
 
-
-
-
 class CourseManager(models.Manager):
 
+    def create_course(self, title, number, credits, subject, gen_eds=[], notes=[], **kwargs):
+        """
+
+        :param title:
+        :param number:
+        :param credits:
+        :param defaults:
+        :param kwargs:
+        :return:
+        """
+        from models import Subject, Note, GenEd
+        subject = Subject.objects.get_or_create(**subject)[0]
+        course = self.create(title=title, number=number, credits=credits, subject=subject, **kwargs)
+        new_m2m_relations = 0
+        new_m2m_relations += db_common_ops.add_kwargs_to_m2m(course.notes, Note.objects, notes)
+        new_m2m_relations += db_common_ops.add_kwargs_to_m2m(course.gen_eds, GenEd.objects, gen_eds)
+        if new_m2m_relations:
+            course.save()
 
 
-    def get_or_create(self, title, code, number,credits, defaults=None, **kwargs):
+    def get_or_create(self, title, code, number, defaults=None, **kwargs):
         """
         Tries to get a course instance based on the subject_code, number and title only.
         If a course instance is found and a non blank description is given; the course
         instance's description will be updated.
         If no instance is found a new course instance is created.
-
+a
         :param title:
         :param code:
         :param number:
@@ -98,17 +116,39 @@ class CourseManager(models.Manager):
         :return:
 
         """
-        from models import Subject, Note, GenEd
+
         try:
             course = self.get(title=title, subject__code=code, number=number)
-            course.update(credits=credits,**kwargs)
-            return course
+            course.update(credits=credits, **kwargs)
+            return course, False
 
         except self.DoesNotExist:
+            course = self.create(title=title, number=number, credits=credits, **kwargs)
+            return course, True
 
-            subject = Subject.get(code=code)
-            course = self.create(title=title, subject=subject, number=number, credits=credits, **kwargs)
-            return course
+
+
+
+class MeetingManager(models.Manager):
+
+    def get_or_create_meeting(self, location, date_period):
+        from models import Location, DatePeriod
+        date_period_obj,date_period_created = DatePeriod.objects.get_or_create(**date_period)
+        location_obj, location_created = Location.objects.get_or_create(**location)
+
+        if date_period and location_created:
+            return self.create(date_period=date_period_obj,location=location), True
+
+        try:
+            meeting = self.get(location=location_obj, date_period=date_period_obj)
+            return meeting, False
+
+        except self.DoesNotExist:
+            return self.create(location=location_obj, date_period=date_period_obj), True
+
+
+
+
 
 
 
