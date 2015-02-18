02409a34d53963e8110544b769502b5386d73dd1
Tanner Baldus <t.baldus285@gmail.com>
2015-02-18 00:14:56 -0800

    migrated to making instructor middle name null and rename geneds in course to gen_eds

---
+++ better_courses/course_search/migrations/0003_auto_20150218_0741.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('course_search', '0002_auto_20150218_0241'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='instructor',
+            name='middle',
+            field=models.CharField(max_length=256, null=True),
+        ),
+    ]
