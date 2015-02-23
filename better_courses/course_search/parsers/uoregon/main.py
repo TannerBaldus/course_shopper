b67640218e2aa363f00a27f129392751d8b56e2d
Tanner Baldus <t.baldus285@gmail.com>
2015-02-22 19:41:05 -0800

    fixed convert_term_to_number to add 0 to the season index, e.g. 20143 to 201403

--- better_courses/course_search/parsers/uoregon/main.py
+++ better_courses/course_search/parsers/uoregon/main.py
@@ -13,7 +13,9 @@ course_search_url = ('http://classes.uoregon.edu/pls/prod/hwskdhnt.P_ListCrse?te
                   '&sel_title=&sel_to_cred=&sel_from_cred=&sel_subj={}&sel_crse=&sel_crn=&sel_camp=%25&sel_levl=%25'
                   '&sel_attr=%25&begin_hh=0&begin_mi=0&begin_ap=a&end_hh=0&end_mi=0&end_ap=a&submit_btn=Show+Classes')
 
-offering_page = 'http://classes.uoregon.edu/pls/prod/hwskdhnt.p_viewdetl?term={}&crn={}'
+offering_page_base = 'http://classes.uoregon.edu/pls/prod/hwskdhnt.p_viewdetl?term={}'
+offering_page_suffix = '&crn={}'
+
 
 subject_code_page = 'http://registrar.uoregon.edu/current-students/subject-codes'
 
@@ -35,19 +37,25 @@ def convert_to_term_number(season, year):
     """
     season = season.lower()
     seasons = ['not_valid', 'fall', 'winter', 'spring', 'summer']
+    season_index = seasons.index(season)
+    assert 1 < season_index < 4, 'season must be fall, winter, spring or summer'
 
     if season != 'fall':         ## All terms except fall use the previous year in its code
         year -= 1
-    return str(year)+str(seasons.index(season.lower()))
+
+    season_code = '0{}'.format(season_index)
+    return str(year)+season_code
 
 
 
-def get_subject_codes():
+def get_subjects():
     """
+
 
 
     :return:
     """
+
     soup = url_to_soup(subject_code_page)
     return parse_subject_table(soup)
 
@@ -63,10 +71,13 @@ def get_offerings(season, year):
     term = convert_to_term_number(season, year)
     subject_codes = [s.code for s in Subject.objects.all()]
     term_search_url = lambda term, code: course_search_url.format(term, code)
+    offering_page_url = offering_page_base.format(term)+offering_page_suffix
 
     for code in subject_codes:
-        results_soup = url_to_soup(term_search_url(term, code))
-        for result in parse_results(results_soup):
+        print code
+        search_url = term_search_url(term,code)
+        for result in parse_results(term_search_url(term, code), offering_page_url):
+            print 'result'
             yield result
 
 
