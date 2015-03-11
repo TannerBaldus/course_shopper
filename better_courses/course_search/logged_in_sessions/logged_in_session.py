47dde447a6237f4b8ca20362b002c0dc2cc05b00
Tanner Baldus <t.baldus285@gmail.com>
2015-03-11 01:58:06 -0700

    added in functuionality to use BeautifulSoup for parsing for speed gains

--- better_courses/course_search/logged_in_sessions/logged_in_session.py
+++ better_courses/course_search/logged_in_sessions/logged_in_session.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from bs4 import BeautifulSoup
 __author__ = 'tanner'
 
 class LoggedInSession(webdriver.Firefox):
@@ -69,6 +70,19 @@ class LoggedInSession(webdriver.Firefox):
         pwd_input = self.find_pwd_element()
         pwd_input.send_keys(pwd)
         self.find_login_button().click()
+
+    def tag_to_soup(self, selenium_tag):
+        """
+        Takes a selenium tag and converts it to a BeautifulSoup object by using the attribute innerHTML.
+        This is useful when parsing large tables as beautiful soup is much faster for parsing.
+        :param selenium_tag: a selenium WebElement with innerHTML
+        :return: a BeautifulSoup object
+        """
+        html = selenium_tag.get_attribute('innerHTML')
+        if not html:
+            raise ValueError('Selenium tag must have innerHTML to be converted to Beautiful Soup object')
+        return BeautifulSoup(html)
+
 
 
 
