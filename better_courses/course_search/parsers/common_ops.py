1c640206d069338de35801ba6742aab0b9676262
Tanner Baldus <t.baldus285@gmail.com>
2015-03-23 22:53:06 -0700

    added flatten dicts and made label rows fns to return an empty list instead of throwing a value error

--- better_courses/course_search/parsers/common_ops.py
+++ better_courses/course_search/parsers/common_ops.py
@@ -1,13 +1,14 @@
 __author__ = 'tanner'
-import string
 from bs4 import BeautifulSoup
+from parser_error import ParserError
 import requests
 
-def label_table_row_data(labels, table_row, text=True):
+def label_table_row_data(labels, table_row, text):
     """
     Creates a dict of labels mapped to text in table data tags contained in a table row tag.
     The order of the labels corresponds to the order of the table data tags. If text is False the mapping will
-    map the tag to the label instead of the tag's text. Example:
+    map the tag to the label instead of the tag's text.
+    Example:
     labels = ['fname','middle','lname']
     <tr>
         <td>Grace</td>
@@ -28,9 +29,13 @@ def label_table_row_data(labels, table_row, text=True):
     if text:
         row_data = [td.text for td in row_data]
 
-    assert len(row_data) != 0, "There is no table data in this row:\n {}".format(table_row)
-    assert len(labels) == len(row_data), "Number td tags and labels must be equal\n labels\n{}\ntd tags\n{}".format(
-        labels, row_data)
+
+    if len(labels) < len(row_data):
+        row_data = row_data[0: len(labels)]
+    if len(labels) > len(row_data):
+        row_data = row_data + ['']*(len(labels)-len(row_data))
+
+
     label_mapping = dict(zip(labels, row_data))
     return label_mapping
 
@@ -46,15 +51,24 @@ def get_labels_from_header(rows, to_lower):
     """
     make_lower = lambda in_str: in_str.lower() if to_lower else in_str
     headers = rows[0].find_all('th')
-    assert headers, 'no header data found in first row.'
+    if not headers:
+        raise ParserError('no header data found in first row.')
     labels = [make_lower(header.text.strip()) for header in headers]
     return labels, rows[1:]
 
 
-def labeled_rows_from_list(lst, labels):
-    return (label_table_row_data(labels, row) for row in lst)
+def labeled_rows_from_list(lst, labels, text=True):
+    """
+    Acts like label_table_row_data except takes in a list of tr tags instead of a BeautifulSoup table tag, as this
+    is sometimes better than looking trying to just use a whole table depending on the site structure.
+    :param lst: a list of BeautifulSoup tr tags
+    :param labels: list of labels in the order corresponding to td tag order
+    :param text: boolean on wheter to use the tag itself or the tag text as values.
+    :return: a dict of label, tag text or tag mappings
+    """
+    return (label_table_row_data(labels, row, text) for row in lst)
 
-def labeled_rows_from_table(table, labels=[], to_lower=True):
+def labeled_rows_from_table(table, labels=[], text=True, to_lower=True):
     """
     Takes a BeautfiulSoup table tag and returns a list of dictonaries mapping the text from td tags to labels from each
     row. These labels can be provided as an argument, if no labels are given the function will try and get labels from
@@ -62,19 +76,21 @@ def labeled_rows_from_table(table, labels=[], to_lower=True):
     columns from left to right. Change to_lower to be false if when getting labels from the header when we want
     to maintain the capatilzation of the header text.
 
-    :param table: a beautfiulsoup table tag
+    :param table: a BeautifulSoup table tag
     :param labels: optional a list of labels to map td text to
     :param to_lower: a boolean indicating if the header text should be lower case default true
     :return: a list of dicts mapping the text from td tags to labels
     """
     if type(table) == list:
-        raise ValueError('table must be a BeautifulSoup obj. For lists of rows use labeled_rows_from_list.')
+        raise ValueError('must be called on a BeautifulSoup obj. For lists of row objs use labeled_rows_from_list.')
 
     rows = table.find_all('tr')
-    assert len(rows) != 0, "There are no rows in this table: {}".format(rows)
+    if len(rows) == 0:
+        raise ParserError("There are no rows in this table: {}".format(rows))
+
     if not labels:
         labels, rows = get_labels_from_header(rows, to_lower)
-    labeled_rows = (label_table_row_data(labels, row) for row in rows)
+    labeled_rows = (label_table_row_data(labels, row, text) for row in rows)
     return labeled_rows
 
 
@@ -122,6 +138,28 @@ def parse_name(full_name):
 
     else:
         fname, lname = full_name
-        middle = None
+        middle = ''
 
     return dict(fname=fname, middle=middle, lname=lname)
+
+
+def flatten_dict_values(input_dict):
+
+    """
+    Flattens a nested dictonares into a list of it's values. Useful for making checking all of the values for some
+    attribute, such as not being an empty string.
+    :param arg:
+    :return:
+    """
+    flat_list = []
+    for value in input_dict.values():
+        if type(value) == dict:
+            flat_list.extend(flatten_dict_values(value))
+        else:
+            flat_list.append(value)
+    return flat_list
+
+
+
+
+
