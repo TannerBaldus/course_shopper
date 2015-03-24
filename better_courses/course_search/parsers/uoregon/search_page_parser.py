b03ff2175c95d4b1d2ae646f44477a1b12a453bd
Tanner Baldus <t.baldus285@gmail.com>
2015-03-23 23:05:08 -0700

    dealt with edge cases in the search page

--- better_courses/course_search/parsers/uoregon/search_page_parser.py
+++ better_courses/course_search/parsers/uoregon/search_page_parser.py
@@ -1,36 +1,43 @@
-from offering_page_parser import get_primary_offering, parse_associated_section
-from better_register.course_search.models import Subject
 __author__ = 'tanner'
-import requests
-from bs4 import BeautifulSoup
+from ..common_ops import url_to_soup
+from offering_page_parser import get_primary_offering, parse_associated_section
+import logging
 
 
+def parse_course_result(table_row):
 
-def parse_course_result(table_row,):
     table_data = table_row.find_all('td')
-    if len(table_data) <= 8:  #if there are 8 entries the class was cancelled
-        return
-    class_type = table_data[0].p.text
+    class_type_row = table_data[0]
     crn = table_data[1].a.text
-    if '+' in class_type:
+    if class_type_row.p and '+' in class_type_row.p.text:
         return 'associated_section', crn
-    return 'course',  crn
+    return 'offering',  crn
 
 
 def get_details(result, detail_url):
-    course_type, crn = result
-    course_page = BeautifulSoup(requests.get(detail_url.format(crn)).text)
-    if course_type == 'course':
-        return dict(course=get_primary_offering(course_page))
-    return dict(associated_section=parse_associated_section(course_page))
+
+    result_type, crn = result
+
+    logging.info('Scraping data from crn: {}'.format(crn))
+    course_page = url_to_soup(detail_url.format(crn))
+
+    if result_type == 'offering':
+        return dict(result_type='offering', data=get_primary_offering(course_page))
+    return dict(result_type='associated_section', data=parse_associated_section(course_page))
 
 
-def parse_results(results_soup):
+def parse_results(results_url, detail_url):
+    results_soup = url_to_soup(results_url)
     result_table = results_soup.find(class_='datadisplaytable')
-    for tr in result_table.find_all('tr'):
+
+    is_good_row = lambda row: len(row.find_all('td', class_='dddefault')) > 8
+    for tr in result_table.find_all(is_good_row):
+        print 'row'
         result = parse_course_result(tr)
-        if result:
-            yield get_details(result)
+        yield get_details(result, detail_url)
+
+
+
 
 
 
